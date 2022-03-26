365871ce2bb4414084f27144497726a2c0dcbbe7
Nithin MSV <nithinmsv@gmail.com>
2022-03-26 10:58:02 +0530

    Post Request Completed

--- src/Components/Home/addProducts.jsx
+++ src/Components/Home/addProducts.jsx
@@ -1,24 +1,67 @@
 import React from "react";
+import axios from "axios";
 
 export default class addProduct extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            productName: "",
+            availableQuantity: 0,
+            loading: true,
+            error: false,
+        };
+      }
+
+        handleProductName = event => {this.setState({ productName: event.target.value })}
+        handleAvailableQuantity = event => {this.setState({ availableQuantity: event.target.value })}
+
+      postData(e) {
+          e.preventDefault()
+
+          const productName  = this.state.productName
+          const availableQuantity = parseInt(this.state.availableQuantity)
+          
+          const product = {
+              productName,
+              availableQuantity
+          }
+
+          axios.post('https://uiexercise.onemindindia.com/api/Product', product)
+          .then(res => {
+            console.log(res);
+            this.setState({ loading: false });
+            e.preventDefault();
+            // this.setState({productName: "", availableQuantity: 0 }) // set state to empty after sumbit
+            this.resetForm();
+          })
+          .catch(err => { // log request error and prevent access to undefined state
+            console.log(err)
+            this.setState({ loading: false, error: true });
+            console.error(err);
+          })
+      }
+
+      resetForm = () => {
+        this.setState({ 
+          productName: "",
+          availableQuantity: ''
+       })
+      }
+
     render() {
         return (
             <div class="d-flex flex-wrap justify-content-around">
-            <div class="card w-50 mt-5">
+            <div class="card w-25 mt-5">
                 <h3 class="mt-3">Add Products</h3>
                 <div class="card-body">
-                <form>
+                <form onSubmit = { this.postData.bind(this) }>
                 <div class="mb-3">
                     <label for="productName" class="form-label">Product Name</label>
-                    <input type="text" class="form-control" id="productName" />
+                    <input type="text" class="form-control" name="productName" value={this.state.productName} id="productName" onChange={this.handleProductName.bind(this)} />
                 </div>
                 <div class="mb-3">
-                    <label for="productId" class="form-label">Product Id</label>
-                    <input type="text" class="form-control" id="productId" />
-                </div>
-                <div class="mb-3">
-                    <label for="availableQuantity" class="form-label">AvailableQuantity</label>
-                    <input type="number" class="form-control" id="availableQuantity" />
+                    <label for="availableQuantity" class="form-label">Available Quantity</label>
+                    <input type="number" class="form-control" name="availableQuantity" value={this.state.availableQuantity} id="availableQuantity" onChange={this.handleAvailableQuantity.bind(this)} />
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
